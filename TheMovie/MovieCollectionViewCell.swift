d961d1b5124622839d1a8ccde89ee4ea48b5e57d
Rafat Touqir Rafsun <Rafsun@rafsuns-macbook-pro.local>
2016-08-16 13:42:39 +0600

    rounded corner

--- TheMovie/MovieCollectionViewCell.swift
+++ TheMovie/MovieCollectionViewCell.swift
@@ -13,13 +13,15 @@ class MovieCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var imageViewMovie: UIImageView!
     
-    override func prepareForReuse() {
-        super.prepareForReuse()
+    override func awakeFromNib() {
+        super.awakeFromNib()
         
-//        self.contentView.layer.cornerRadius = 2.0;
-//        self.contentView.layer.borderWidth = 1.0;
-//        self.contentView.layer.borderColor = UIColor.clearColor().CGColor;
-//        self.contentView.layer.masksToBounds = true;
+        self.layer.cornerRadius = 10
+        self.layer.masksToBounds = true
+//        self.imageViewMovie.layer.cornerRadius = 10
+//        self.imageViewMovie.layer.masksToBounds = true
+//        self.imageViewMovie.clipsToBounds = true
+//        self.clipsToBounds = true
         
 //        self.layer.shadowColor = UIColor.whiteColor().CGColor;
 //        self.layer.shadowOffset = CGSizeMake(0, 7.0);
@@ -27,6 +29,17 @@ class MovieCollectionViewCell: UICollectionViewCell {
 //        self.layer.shadowOpacity = 1.0;
 //        self.layer.masksToBounds = true;
 //        self.layer.shadowPath = UIBezierPath(roundedRect:self.bounds, cornerRadius:self.contentView.layer.cornerRadius).CGPath;
+
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+//        self.contentView.layer.cornerRadius = 2.0;
+//        self.contentView.layer.borderWidth = 1.0;
+//        self.contentView.layer.borderColor = UIColor.clearColor().CGColor;
+//        self.contentView.layer.masksToBounds = true;
+        
     }
     
     
